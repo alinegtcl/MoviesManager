3207d99b41fbd279075de7df6af7ebfe4a9fdea3
aline <linegcruz@gmail.com>
2023-12-09 19:54:20 -0300

    feat(navigation): added nav graph, nav host, initial destination and theme colors.

--- app/src/main/java/com/luisitolentino/moviesmanager/presentation/view/activity/MoviesManagerActivity.kt
+++ app/src/main/java/com/luisitolentino/moviesmanager/presentation/view/activity/MoviesManagerActivity.kt
@@ -2,6 +2,12 @@ package com.luisitolentino.moviesmanager.presentation.view.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.luisitolentino.moviesmanager.R
 import com.luisitolentino.moviesmanager.databinding.ActivityMoviesManagerBinding
 
 class MoviesManagerActivity : AppCompatActivity() {
@@ -10,9 +16,24 @@ class MoviesManagerActivity : AppCompatActivity() {
         ActivityMoviesManagerBinding.inflate(layoutInflater)
     }
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbarMoviesManager)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerMoviesManager) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragmentContainerMoviesManager)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
 }
---
+++ app/src/main/java/com/luisitolentino/moviesmanager/presentation/view/fragment/ListMoviesFragment.kt
@@ -0,0 +1,27 @@
+package com.luisitolentino.moviesmanager.presentation.view.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.luisitolentino.moviesmanager.databinding.FragmentListMoviesBinding
+
+class ListMoviesFragment : Fragment() {
+
+    private var _binding: FragmentListMoviesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentListMoviesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
