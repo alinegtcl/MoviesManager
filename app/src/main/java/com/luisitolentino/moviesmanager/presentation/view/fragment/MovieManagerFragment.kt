4e7b0f0ff6fbb77bec7a74398fc19a925d393cc3
t737070 <t737070@f1rst.com.br>
2023-12-12 16:26:15 -0300

    feat(genre_spinner): added handling for configuration and selection of movie genres.

--- app/src/main/java/com/luisitolentino/moviesmanager/presentation/view/fragment/MovieManagerFragment.kt
+++ app/src/main/java/com/luisitolentino/moviesmanager/presentation/view/fragment/MovieManagerFragment.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.luisitolentino.moviesmanager.databinding.FragmentMovieManagerBinding
 import com.luisitolentino.moviesmanager.domain.model.Movie
@@ -19,6 +23,12 @@ class MovieManagerFragment : Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var movie: Movie
+    private val movieGenreList = listOf<MovieGenre>(
+        MovieGenre(1L, "Comédia"),
+        MovieGenre(2L, "Drama"),
+        MovieGenre(3L, "Terror")
+    )
+    private var genreSelected: MovieGenre? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,7 +40,39 @@ class MovieManagerFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupSpinnerMovieGenre()
         setupAddMovie()
+    }
+
+    private fun setupSpinnerMovieGenre() {
+        val adapter = ArrayAdapter(
+            requireContext(),
+            org.koin.android.R.layout.support_simple_spinner_dropdown_item,
+            movieGenreList.map { it.description }
+        )
+        binding.spinnerMovieGenre.adapter = adapter
+        binding.spinnerMovieGenre.itemSelected { genre ->
+            movieGenreList.forEach {
+                if (it.description == genre)
+                    genreSelected = it
+            }
+        }
+    }
+
+    private fun Spinner.itemSelected(action: (genre: String) -> Unit) {
+        this.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                action(parent?.getItemAtPosition(position) as String)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+        }
     }
 
     private fun setupAddMovie() {
@@ -42,10 +84,11 @@ class MovieManagerFragment : Fragment() {
                     editTextMovieStudio.text.toString(),
                     editTextMovieDuration.text.toString().toInt(),
                     checkMovieWatched.isChecked,
-                    MovieGenre(1L, "comedia"),
+                    genreSelected!!,
                     editTextMovieScore.text.toString().toInt()
                 )
             }
+            Toast.makeText(requireContext(), "$genreSelected", Toast.LENGTH_SHORT).show()
         }
     }
 
